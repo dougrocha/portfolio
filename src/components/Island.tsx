40d5f8459962093fb75518e9f999a79d203e1b99
Douglas Rocha <79188161+slashtp3@users.noreply.github.com>
2022-01-18 00:25:12 -0500

    chore(favicon icon): changed favicon icon and pushed island obj

---
+++ src/components/Island.tsx
@@ -0,0 +1,27 @@
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+import IslandAsset from '../assets/Island'
+
+export const Island = () => {
+  return (
+    <Canvas camera={{ position: [0, 5, 15] }}>
+      <IslandComponent />
+    </Canvas>
+  )
+}
+
+const IslandComponent = (props: JSX.IntrinsicElements['mesh']) => {
+  return (
+    <>
+      <ambientLight intensity={0.6} />
+      <pointLight position={[10, 10, 10]} />
+      <Suspense fallback={null}>
+        <IslandAsset {...props} />
+      </Suspense>
+      <OrbitControls enableZoom={false} enableDamping />
+    </>
+  )
+}
+
+export default Island
