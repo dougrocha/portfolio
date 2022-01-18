57efe2d118153ada801d150a11781ed0ba3897a4
Douglas Rocha <79188161+slashtp3@users.noreply.github.com>
2022-01-18 00:27:09 -0500

    refactor(index.tsx): refactored index page with layout and seo things

---
+++ src/layouts/default.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import Contact from '../components/contact'
+import NavBar from '../components/navbar'
+import { LayoutProps } from '../utils/types'
+
+const DefaultLayout = ({ children, title }: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>{title || 'Douglas Rocha | Portfolio'}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="My portfolio. Check out some of my projects and contact me if you like them."
+        />
+        <meta
+          property="og:title"
+          content={title || 'Douglas Rocha | Portfolio'}
+        />
+        <meta
+          property="og:description"
+          content="My portfolio. Check out some of my projects and contact me if you like them."
+        />
+        <meta property="og:url" content="https://dougrocha.com/" />
+        <meta property="og:type" content="website" />
+      </Head>
+
+      <div className="block min-h-screen overflow-hidden antialiased text-white bg-bg">
+        <NavBar />
+        {children}
+        <Contact />
+      </div>
+    </>
+  )
+}
+
+export default DefaultLayout
